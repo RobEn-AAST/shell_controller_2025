0e1575b9ca827ee59b675d3faeb4d94c16bc5eb2
zeyad-shaban <zeyadshapan2004@gmail.com>
2025-04-26 07:34:34 +0300

    fixed actorr clearing and aded study

--- roben_ai/ai_src/gym_env_test.py
+++ roben_ai/ai_src/gym_env_test.py
@@ -1,16 +1,17 @@
 import gym_carla
 import gymnasium as gym
 
+
 if __name__ == "__main__":
-    env, obs_space = gym_carla.make_carla_env()
+    env = gym_carla.make_carla_env()
 
     obs = env.reset()
     while True:
         action = [2, 0.0]
         obs, r, terminated, truncated, _ = env.step(action)
         done = terminated or truncated
-
+        print('epic stuff lol')
         if done:
             obs = env.reset()
-            
+
 # test content
--- roben_ai/ai_src/main.py
+++
@@ -1,9 +0,0 @@
-from gym_carla import make_carla_env
-from stable_baselines3 import PPO
-from gymnasium import spaces
-
-env, obs_space = make_carla_env()
-
-
-if __name__ == '__main__':
-    
