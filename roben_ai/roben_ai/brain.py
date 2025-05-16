9b86504c20a44f4a4dc00f144ac2aa12785b4881
zeyad-shaban <zeyadshapan2004@gmail.com>
2025-05-16 20:26:54 +0300

    added basis for pid controller and added carla api directly

--- roben_ai/roben_ai/brain.py
+++ roben_ai/roben_ai/brain.py
@@ -1,84 +1,320 @@
-#!/usr/bin/env python3
+import math                                                           # for trigonometry, hypot
+import time                                                           # for sleep delays
 
-import rclpy
-from rclpy.node import Node
+import rclpy                                                          # ROS 2 Python client library :contentReference[oaicite:0]{index=0}
+from rclpy.node import Node                                           # base class for ROS 2 nodes :contentReference[oaicite:1]{index=1}
+
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped   # for /initialpose and /goal_pose topics :contentReference[oaicite:2]{index=2}
+from nav_msgs.msg import Path, Odometry                               # for /carla/.../waypoints and odometry :contentReference[oaicite:3]{index=3}
+
+# CARLA Python API
+from carla import Client, Transform, Location, Rotation, Color
+from carla.ros2.types import CarlaEgoVehicleControl
+from carla.agents.navigation.controller import VehiclePIDController
+from carla.agents.navigation.global_route_planner import GlobalRoutePlanner
+# from carla.LaneMarkingColor import GREEN
 import carla
-import numpy as np
-from car_dreamer.toolkit.planner.agents.navigation.global_route_planner import GlobalRoutePlanner
-from car_dreamer.toolkit.planner.agents.navigation.basic_agent import BasicAgent
-from ai_src.navigator.wp_utils import xyz_to_right_lane
-from ai_src.navigator.tsp_solver import optimize_route_order
-from ai_src.navigator.route_generator import generate_full_route
 
-# pytorch stablebaseline gymansium
+# TF conversions
+from transforms3d.euler import quat2euler
+
+
 class Brain(Node):
     def __init__(self):
-        """
-        The main controller of them all, the ultimate god of this body!
-        Takes the image, and any other sensors we could find,
-        sends them to an actor critic policy, and retreives the action we are asked to execute
-        And executes it! voalla!!
-        """
-        super().__init__("brain")
-
-        client = carla.Client("localhost", 2000) # type: ignore
-        client.set_timeout(20)
-        world = client.get_world()
-        carla_map = world.get_map()
-        ego_vehicle = next(v for v in world.get_actors().filter("vehicle.*") if v.attributes.get("role_name") == "ego_vehicle")
-
-        target_points = [
-            [280.363739, 133.306351, 0.001746],
-            [334.949799, 161.106171, 0.001736],
-            [339.100037, 258.568939, 0.001679],
-            [396.295319, 183.195740, 0.001678],
-            [267.657074, 1.983160, 0.001678],
-            [153.868896, 26.115866, 0.001678],
-            [290.515564, 56.175072, 0.001677],
-            [92.325722, 86.063644, 0.001677],
-            [88.384346, 287.468567, 0.001728],
-            [177.594101, 326.386902, 0.001677],
-            [-1.646942, 197.501282, 0.001555],
-            [59.701321, 1.970804, 0.001467],
-            [122.100121, 55.142044, 0.001596],
-            [161.030975, 129.313187, 0.001679],
-            [184.758713, 199.424271, 0.001680],
-        ]
-        sampling_resolution = 1.0
-
-        target_locations = xyz_to_right_lane(target_points, carla_map)
-
-        grp = GlobalRoutePlanner(carla_map, sampling_resolution)
-
-        optimized_targets = optimize_route_order(
-            ego_vehicle.get_location(),
-            target_locations,
-            grp,
+        super().__init__('carla_route_planner')
+ 
+ 
+        # Subscribers
+        self.initialpose_sub = self.create_subscription(
+            PoseWithCovarianceStamped,
+            '/initialpose',
+            self.initialpose_callback,
+            10
         )
-
-        full_route = generate_full_route(ego_vehicle.get_location(), optimized_targets, grp)
-
-        # ======= MOVE VEHICLE ========
-        agent = BasicAgent(ego_vehicle)
-        agent.ignore_traffic_lights(True)
-        agent.set_target_speed(45)
-        agent.set_global_plan(full_route)
-
-        self.get_logger().info("Brain node started... about to move")
+        self.goal_pose_sub = self.create_subscription(
+            PoseStamped,
+            '/goal_pose',
+            self.goal_pose_callback,
+            10
+        )
+        self.waypt_sub = self.create_subscription(
+            Path, 
+            '/carla/ego_vehicle/waypoints', 
+            self.waypoints_callback, 
+            10
+        )
+        # Subscriber to the /carla/ego_vehicle/odometry topic
+        self.odom_sub = self.create_subscription(
+            Odometry,
+            '/carla/ego_vehicle/odometry',
+            self.odometry_callback,
+            10)
+ 
+ 
+        self.vehicle_control_publisher = self.create_publisher(CarlaEgoVehicleControl, 
+                                                      '/carla/ego_vehicle/vehicle_control_cmd', 
+                                                      10)
+     
+        # Initialize Carla client and map
+        self.client = Client('localhost', 2000)
+        self.client.set_timeout(10.0)
+ 
+        # Get the current world
+        self.world = self.client.get_world()
+ 
+ 
+        # Check if Town01 is already loaded
+        if 'Town01' not in self.world.get_map().name:
+            print("Town01 is not loaded. Loading Town01...")
+            self.world = self.client.load_world('Town01')
+            print("Done!")
+        else:
+            print("Town01 is already loaded.")
+ 
+ 
+        self.map = self.world.get_map()
+        # Initialize GlobalRoutePlanner
+        self.route_planner = GlobalRoutePlanner(self.map, 2.0)
+ 
+        # Get all actors (vehicles, pedestrians, etc.) in the world
+        self.actors = self.world.get_actors()
+ 
+        # Filter to get only the vehicles get the 0-th veh as there is only one veh
+        self.vehicle = self.actors.filter('vehicle.*')[0]
+ 
+        # Placeholders for start and end poses
+        self.start_pose = None
+        self.end_pose = None
+        self.waypoints_list = []
+        self.odom = None
+ 
+        # TF2 listener and buffer
+        # self.tf_buffer = Buffer()
+        # self.tf_listener = TransformListener(self.tf_buffer, self)
+        # self.vehicle_loc = None
+ 
+    def odometry_callback(self, msg):
+         
+         
+        self.get_logger().info(f"Received odometry data: {msg.pose.pose.position.x}, {msg.pose.pose.position.y}, {msg.pose.pose.position.z}")
+ 
+        # Extract position and orientation from Odometry message
+        x = msg.pose.pose.position.x
+        y = -msg.pose.pose.position.y
+        z = msg.pose.pose.position.z
+         
+        print(" ^^^^ ODOM XYZ: ", x,y,z )
+ 
+        orientation_q = msg.pose.pose.orientation
+        roll, pitch, yaw = quat2euler(
+            [orientation_q.w, orientation_q.x, orientation_q.y, orientation_q.z])
+ 
+        # Create a carla.Location object
+        location = carla.Location(x=x, y=y, z=z)
+ 
+        # Create a carla.Rotation object
+        rotation = carla.Rotation(roll=roll, pitch=pitch, yaw=yaw)
+ 
+        # Create a carla.Transform object
+        transform = carla.Transform(location, rotation)
+         
+        self.odom = transform
+ 
+    def waypoints_callback(self, msg):
+        # self.waypoints_list.clear()  # Clear the list before storing new waypoints
+ 
+        # Iterate through all the waypoints in the Path message
+        for pose in msg.poses:
+            # Extract the position from the pose
+            x = pose.pose.position.x
+            y = -pose.pose.position.y
+            z = pose.pose.position.z
+ 
+            # Extract the orientation (quaternion) from the pose
+            orientation_q = pose.pose.orientation
+            roll, pitch, yaw = quat2euler(
+                [orientation_q.w, orientation_q.x, orientation_q.y, orientation_q.z])
+ 
+            # Create a carla.Location object
+            location = carla.Location(x=x, y=y, z=z)
+ 
+            # Create a carla.Rotation object
+            rotation = carla.Rotation(roll=roll, pitch=pitch, yaw=yaw)
+ 
+            # Create a carla.Transform object
+            transform = carla.Transform(location, rotation)
+ 
+            # Store the Waypoint in the global list
+            self.waypoints_list.append(transform)
+             
+            self.get_logger().info(f"Stored {len(self.waypoints_list)} waypoints as carla.libcarla.Waypoint objects.")
+ 
+    def create_ctrl_msg(self, throttle, steer, brake):
+        control_msg = CarlaEgoVehicleControl()
+        control_msg.throttle = throttle
+        control_msg.steer = steer
+        control_msg.brake = brake
+        return control_msg
+ 
+    def initialpose_callback(self, msg):
+        self.get_logger().info("Received initialpose")
+        self.start_pose = msg.pose.pose
+ 
+    def goal_pose_callback(self, msg):
+        self.get_logger().info("Received goal_pose")
+        self.end_pose = msg.pose
+        # Clear the waypoints list for the new goal
+        self.waypoints_list.clear()
+    def get_transform(self, vehicle_location, angle, d=6.4):
+        a = math.radians(angle)
+        location = carla.Location(
+            d * math.cos(a), d * math.sin(a), 2.0) + vehicle_location
+        return carla.Transform(location, carla.Rotation(yaw=180 + angle, pitch=-15))
+ 
+    def setup_PID(self, vehicle):
+        """
+        This function creates a PID controller for the vehicle passed to it 
+        """
+        args_lateral_dict = {
+            'K_P': 0.5,  # Reduced proportional gain for smoother steering
+            'K_D': 0.1,  # Small derivative gain to dampen oscillations
+            'K_I': 0.01, # Small integral gain to correct for long-term drift
+            'dt': 0.05
+        }
+ 
+        args_long_dict = {
+            'K_P': 0.2,  # Slightly lower gain for acceleration control
+            'K_D': 0.3,  # Moderate derivative gain
+            'K_I': 0.01, # Small integral gain
+            'dt': 0.05
+        }
+ 
+        PID= VehiclePIDController(vehicle,args_lateral=args_lateral_dict,args_longitudinal=args_long_dict)
+     
+        return PID
+     
+     
+    def find_dist_veh(self, vehicle_loc,target):
+        dist = math.sqrt( (target.location.x - vehicle_loc.x)**2 + \
+                         (target.location.y - vehicle_loc.y)**2 )
+         
+        return dist
+ 
+ 
+    def drive_through_plan(self, planned_route, vehicle, speed, PID):
+        """
+        This function drives throught the planned_route with the speed passed in the argument
+         
+        """
+         
+        i=0
+        waypt_cnt = len(planned_route)-1
+        target=planned_route[0]
+ 
+        cnt = 0
         while True:
-            control = agent.run_step()  # Auto-generates throttle/brake/steering
-            ego_vehicle.apply_control(control)
-
-
+             
+            self.world.get_spectator().set_transform(self.get_transform(vehicle.get_location() +
+                                                                carla.Location(z=1, x=0.5), 
+                                                                vehicle.get_transform().rotation.yaw-180))
+             
+            # vehicle_loc =  vehicle.get_location()
+            vehicle_loc =  self.odom.location
+ 
+            distance_v = self.find_dist_veh(vehicle_loc,target)
+             
+ 
+            control = PID.run_step(speed,target)
+            # vehicle.apply_control(control)
+            ctrl_msg = self.create_ctrl_msg(control.throttle,
+                                            control.steer,
+                                            control.brake)
+            self.vehicle_control_publisher.publish(ctrl_msg)
+             
+            if i==(len(planned_route)-1):
+                print("last waypoint reached")
+                break
+             
+             
+            if (distance_v<3.5):
+                 
+                control = PID.run_step(speed,target)
+                # vehicle.apply_control(control)
+                ctrl_msg = self.create_ctrl_msg(control.throttle,
+                            control.steer,
+                            control.brake)
+                self.vehicle_control_publisher.publish(ctrl_msg)
+                i=i+1
+                target=planned_route[i]
+ 
+            GREEN = "GREEN"
+            RESET = "RESET"
+            if cnt%5==0:
+                print("=----------------------------------------------------------")
+                print(f"\n{GREEN} ***** from current loc to {i}/{waypt_cnt} waypoint distance: {distance_v}{RESET}\n")
+                print("ROS2 vehilce location: ", self.odom.location)
+                print("CARLA vehilce location: ", vehicle.get_location())
+                print("target location: ", target.location)
+                rclpy.spin_once(self)
+             
+            # time.sleep(0.1)  # Add a slight delay to reduce control frequency
+            # time.sleep(1)  # Add a 1-second delay
+             
+            # print("throttle: ", control.throttle)
+            cnt+=1
+             
+        control = PID.run_step(0,planned_route[len(planned_route)-1])
+        # vehicle.apply_control(control)
+        ctrl_msg = self.create_ctrl_msg(control.throttle,
+                            control.steer,
+                            control.brake)
+        self.vehicle_control_publisher.publish(ctrl_msg)
+ 
+ 
+    def run(self):
+        desired_velocity=10 #Km/h
+ 
+        while rclpy.ok():
+            rclpy.spin_once(self)
+             
+            if self.start_pose is None or self.end_pose is None:
+                self.get_logger().info(f'Start pose: {self.start_pose}, End pose: {self.end_pose}')
+             
+            elif not self.waypoints_list:
+                self.get_logger().info('Waiting for waypoints to be generated...')
+             
+            else:
+                # Delay to ensure waypoints are populated
+                self.get_logger().info('Waiting a bit for waypoints to be fully populated...')
+                time.sleep(1)  # Add a 1-second delay
+                self.get_logger().info(f'Generated {len(self.waypoints_list)} waypoints from start to end pose')
+                 
+ 
+                # calculating life time of the marker
+                total_dist = self.find_dist_veh(self.waypoints_list[0].location, self.waypoints_list[len(self.waypoints_list)-1])
+                marker_life_time = (total_dist/desired_velocity) * 3.6
+                 
+ 
+                # Draw waypoints on the Carla map
+                for w in self.waypoints_list:
+                    # print("self.waypoints_list: ",w.location)
+                    self.world.debug.draw_string(w.location, 'O', draw_shadow=False,
+                                                 color=Color(r=255, g=0, b=0), life_time=5000000.0,
+                                                 persistent_lines=True)
+                 
+                 
+                # drive the vehicle
+                PID=self.setup_PID(self.vehicle)
+                self.drive_through_plan(self.waypoints_list,self.
+                                        vehicle,desired_velocity,
+                                        PID)
+                 
+ 
+                # After processing, break the loop if needed
+                break
+ 
 def main(args=None):
     rclpy.init(args=args)
-
-    node = Brain()
-    rclpy.spin(node)
-
-    node.destroy_node()
+    route_planner = Brain()
+    route_planner.run()
     rclpy.shutdown()
-
-
-if __name__ == "__main__":
-    main()
